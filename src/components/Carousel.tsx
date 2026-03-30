fb64f905f9c520920e3891e60b26c68dc7538386
anastasiiadns <nastyadenisenko17@gmail.com>
2026-03-30 14:27:36 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
+import { State } from '../App';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+export const Carousel: React.FC<State> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0); // де зараз знаходиться карусель
+  const maxIndex = images.length - frameSize; // докуди максимум можна дійти
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  const nextButton = () => {
+    if (infinite) {
+      setCurrentIndex(prev => (prev + step) % images.length);
+    } else {
+      setCurrentIndex(prev => Math.min(prev + step, maxIndex));
+    }
+  };
+
+  const prevButton = () => {
+    if (infinite) {
+      setCurrentIndex(prev => (prev - step + images.length) % images.length);
+    } else {
+      setCurrentIndex(prev => Math.max(prev - step, 0));
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="Carousel"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+          overflow: 'hidden',
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${currentIndex * itemWidth}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {images.map((src, image) => (
+            <li key={image} className="Carousel__item">
+              <img
+                src={src}
+                alt={`${image + 1}`}
+                className="Carousel__images"
+                width={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button type="button" onClick={prevButton}>
+          Prev
+        </button>
+        <button type="button" onClick={nextButton} data-cy="next">
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
